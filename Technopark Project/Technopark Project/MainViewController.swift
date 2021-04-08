d645cdddc6872be16c64b1ed0644ac9e5eb6b786
Vyacheslav Pronin <vyacheslavpronin@MacBook-Pro-Vyacheslav.local>
2021-04-08 18:10:34 +0300

    New name view controller

--- Technopark Project/Technopark Project/MainViewController.swift
+++ Technopark Project/Technopark Project/MainViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import PinLayout
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
