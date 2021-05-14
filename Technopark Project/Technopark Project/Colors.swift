d379fb35079420feba092c792ea1e8cb10abb14b
Vyacheslav <pronin.slavok@mail.ru>
2021-05-14 07:54:50 +0300

    добавлены иконки и цвета

---
+++ Technopark Project/Technopark Project/Colors.swift
@@ -0,0 +1,22 @@
+//
+//  Colors.swift
+//  Technopark Project
+//
+//  Created by Vyacheslav Pronin on 14.05.2021.
+//
+
+import Foundation
+
+enum Color: String {
+    case button = "9CB9D1"
+    case text = "E0E9F2"
+    case icon
+    case view
+    case underline
+    case backGroundMain = "313844"
+    case backGroundOther = "7087A4"
+    case backGroundSetting = "597387"
+    case error = "cc0000"
+    case success = "3A9063"
+    case other = "f0ff00"
+}
